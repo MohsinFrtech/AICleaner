18d35e08f4f91af9c02aec02f729cdc1e80966c2
UmerHamza <umer.hamza@frtech.org>
2024-05-22 12:31:52 +0500

    committed video folder.

--- app/src/main/java/com/example/aicleaner/ui/fragments/DeleteVideoFragment.kt
+++ app/src/main/java/com/example/aicleaner/ui/fragments/DeleteVideoFragment.kt
@@ -1,20 +1,33 @@
 package com.example.aicleaner.ui.fragments
 
-import android.content.Intent
-import android.os.storage.StorageManager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.aicleaner.R
+import com.example.aicleaner.databinding.DeleteVideoFragmentBinding
 
 
 class DeleteVideoFragment:Fragment() {
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//
-//    }
 
+    private var bindingDeleteVideo:DeleteVideoFragmentBinding?=null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val layDelete = inflater.inflate(R.layout.delete_video_fragment,container,false)
+        bindingDeleteVideo=DataBindingUtil.bind(layDelete)
+        showAllVideosInParticularFolder()
+        return layDelete
+    }
+
+    //Show all videos in particular folder....
+    private fun showAllVideosInParticularFolder() {
+
+    }
 
 
 }
